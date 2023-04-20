52c4fcbb0e71c967daf95cf7dc7dbaf0df84f72b
mason26611 <masong1508@gmail.com>
2023-04-19 21:55:40 -0400

    Add satiety enchantment & start replenish

--- src/main/java/perhaps/potatosmpx/enchantment/enchantments/ReplenishEnchantment.java
+++ src/main/java/perhaps/potatosmpx/enchantment/enchantments/ReplenishEnchantment.java
@@ -1,12 +1,30 @@
 package perhaps.potatosmpx.enchantment.enchantments;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.lwjgl.system.CallbackI;
+
+import java.util.List;
+import java.util.Map;
 
 public class ReplenishEnchantment extends Enchantment {
     public ReplenishEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
         super(pRarity, pCategory, pApplicableSlots);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
@@ -22,5 +40,42 @@ public class ReplenishEnchantment extends Enchantment {
     @Override
     public int getMaxCost(int level) {
         return this.getMinCost(level) + 50;
+    }
+
+    private final Map<Block, Item> seedBlocks = Map.of(
+            Blocks.WHEAT, Items.WHEAT_SEEDS,
+            Blocks.CARROTS, Items.CARROT,
+            Blocks.POTATOES, Items.POTATO,
+            Blocks.BEETROOTS, Items.BEETROOT_SEEDS,
+            Blocks.NETHER_WART, Items.NETHER_WART
+    );
+
+    @SubscribeEvent
+    public void onBlockBreak(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+        Level world = player.level;
+        ItemStack heldItem = player.getMainHandItem();
+        if (heldItem.isEmpty()) return;
+
+        int level = EnchantmentHelper.getItemEnchantmentLevel(this, heldItem);
+        if (level <= 0 || world.isClientSide) return;
+
+        BlockState state = event.getState();
+        Block block = state.getBlock();
+        List<ItemStack> drops = Block.getDrops(state, (ServerLevel) world, event.getPos(), null, player, heldItem);
+        if (!seedBlocks.containsKey(block)) return;
+
+        boolean hasSeed = false;
+        Item seed = seedBlocks.get(block);
+        for (ItemStack drop : drops) {
+            System.out.println("checking....");
+            if (drop.getItem() != seed) continue;
+            drops.remove(drop);
+            hasSeed = true;
+            break;
+        }
+
+        // TODO: This doesn't work yet
+        if (hasSeed) world.setBlock(event.getPos(), block.defaultBlockState(), 3);
     }
 }
--- src/main/java/perhaps/potatosmpx/enchantment/enchantments/SatietyEnchantment.java
+++ src/main/java/perhaps/potatosmpx/enchantment/enchantments/SatietyEnchantment.java
@@ -1,12 +1,22 @@
 package perhaps.potatosmpx.enchantment.enchantments;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class SatietyEnchantment extends Enchantment {
     public SatietyEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
         super(pRarity, pCategory, pApplicableSlots);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
@@ -22,5 +32,19 @@ public class SatietyEnchantment extends Enchantment {
     @Override
     public int getMaxCost(int level) {
         return this.getMinCost(level) + 50;
+    }
+
+    @SubscribeEvent
+    public void onLivingDeath(LivingDeathEvent event) {
+        Entity target = event.getEntityLiving();
+        Entity attacker = event.getSource().getDirectEntity();
+        if (!(attacker instanceof Player player) || target instanceof Animal) return;
+
+        Level world = player.level;
+        ItemStack heldItem = player.getMainHandItem();
+        int level = EnchantmentHelper.getItemEnchantmentLevel(this, heldItem);
+        if (level <= 0 || world.isClientSide || heldItem.isEmpty() || world.getRandom().nextFloat() > (0.10f * level)) return;
+
+        player.getFoodData().eat(3, 0.5f);
     }
 }
